dc9c5d6123c0a5478fff5e16ffcb794cc578edd1
BossMan1994 <BossMan1994@users.noreply.github.com>
2017-12-04 11:57:20 +0530

    Unity Idle Clicker Part-2

---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+	public Slider progressSlider;
+	public Text currentBalance;
+
+	public int storeTimer;
+	public int profit;
+
+	public int balance;
+
+	private bool startTimer;
+	private float currentTimer;
+
+	// Use this for initialization
+	void Start () {
+		currentBalance.text = "$" + balance.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTimer) {
+			currentTimer = currentTimer + Time.deltaTime;
+			progressSlider.value = currentTimer / storeTimer;
+			if (currentTimer > storeTimer) {
+				startTimer = false;
+				currentBalance.text = "$" + balance.ToString ();
+				progressSlider.value = 0;
+			}
+		}
+	}
+
+	public void clickme() {
+		balance = balance + 1;
+		currentTimer = 0;
+		startTimer = true;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Unity Idle Clicker
+
+In this series, we will be creating an incremental idle clicker game using unity in C#. This series will give you a good introduction to Unity UI and C# coding in unity. 
+
+In this game, you will start with 1 shop and the aim is to get richer and richer. The more money you have the more shops you can unlock. You can hire managers to do you work and even buy upgrades to upgrade your shop to earn more money.
+
+# View Tutorials
+
+Idle Clicker Game - Unity - C# - Part-1 - Introduction - https://youtu.be/MW2nir0W_OE
+
+Idle Clicker Game - Unity - C# - Part-2 - https://youtu.be/E32JQ4bTIQ0
+
+# Contact
+Feel free to contact me through my website http://learningfromscratch.online/
+>If you are following this tutorial series I would appreciate if you can subscribe and like my videos as it will keep me motivated to share my knowledge with you all!
